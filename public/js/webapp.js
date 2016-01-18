1dc732fc9904751ed6eccada7a34ef7f299e89a5
Kanstantsin Shydla <Kanstantsin_Shydla@epam.com>
2016-01-18 19:55:02 +0300

    #Layers animation.#login form

--- public/js/webapp.js
+++ public/js/webapp.js
@@ -2,6 +2,71 @@ $(document).ready(function() {
     init();
 });
 
+function getNewsData() {
+    return [
+        {id: getRandomString(), title: 'REVIEW: ROCKY MOUNTAIN ALTITUDE 770 MSL', shortText: '‘Trail’ or ‘all-mountain’ is a matter of perspective',
+            size: 4, image: "BIKP-160200-TRAIL-051-640x360.jpg", date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'Where’s the Love?', shortText: 'Why there are no hardtails in the Bible of Bike Tests',
+            size: 4, image: "52A77841-2-640x360.jpg", date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'DEITY: NEKO MULALLY', shortText: 'Mulally snags some liquid courage',
+            size: 2, image: 'Screen-Shot-2016-01-13-at-7.17.36-AM-320x180.png', date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'COLD CASE', shortText: 'The country\'s first urban bike park seeks a second coming',
+            size: 2, image: "BIKP-150600-PARK-06-300x169.jpg", date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'Trail', shortText: 'trail all mountain cross country womens all gear',
+            size: 2, image: "sidebar-bible-img.jpg", date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'PURE METAL', shortText: 'Durango Bike Company\'s newest model isn\'t carbon',
+            size: 2, image: "ticker-15-300x169.jpg", date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'REVIEW: ROCKY MOUNTAIN ALTITUDE 770 MSL', shortText: '‘Trail’ or ‘all-mountain’ is a matter of perspective',
+            size: 4, image: "BIKP-160200-TRAIL-051-640x360.jpg", date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'Where’s the Love?', shortText: 'Why there are no hardtails in the Bible of Bike Tests',
+            size: 4, image: "52A77841-2-640x360.jpg", date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'Trail', shortText: 'trail all mountain cross country womens all gear',
+            size: 2, image: "sidebar-bible-img.jpg", date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'PURE METAL', shortText: 'Durango Bike Company\'s newest model isn\'t carbon',
+            size: 2, image: "ticker-15-300x169.jpg", date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'REVIEW: ROCKY MOUNTAIN ALTITUDE 770 MSL', shortText: '‘Trail’ or ‘all-mountain’ is a matter of perspective',
+            size: 4, image: "BIKP-160200-TRAIL-051-640x360.jpg", date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'REVIEW: ROCKY MOUNTAIN ALTITUDE 770 MSL', shortText: '‘Trail’ or ‘all-mountain’ is a matter of perspective',
+            size: 4, image: "BIKP-160200-TRAIL-051-640x360.jpg", date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'Where’s the Love?', shortText: 'Why there are no hardtails in the Bible of Bike Tests',
+            size: 4, image: "52A77841-2-640x360.jpg", date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'DEITY: NEKO MULALLY', shortText: 'Mulally snags some liquid courage',
+            size: 2, image: 'Screen-Shot-2016-01-13-at-7.17.36-AM-320x180.png', date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'COLD CASE', shortText: 'The country\'s first urban bike park seeks a second coming',
+            size: 2, image: "BIKP-150600-PARK-06-300x169.jpg", date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'Trail', shortText: 'trail all mountain cross country womens all gear',
+            size: 2, image: "sidebar-bible-img.jpg", date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'PURE METAL', shortText: 'Durango Bike Company\'s newest model isn\'t carbon',
+            size: 2, image: "ticker-15-300x169.jpg", date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'REVIEW: ROCKY MOUNTAIN ALTITUDE 770 MSL', shortText: '‘Trail’ or ‘all-mountain’ is a matter of perspective',
+            size: 4, image: "BIKP-160200-TRAIL-051-640x360.jpg", date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'Where’s the Love?', shortText: 'Why there are no hardtails in the Bible of Bike Tests',
+            size: 4, image: "52A77841-2-640x360.jpg", date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'Trail', shortText: 'trail all mountain cross country womens all gear',
+            size: 2, image: "sidebar-bible-img.jpg", date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'PURE METAL', shortText: 'Durango Bike Company\'s newest model isn\'t carbon',
+            size: 2, image: "ticker-15-300x169.jpg", date: 'Jan 01 2016'},
+        {id: getRandomString(), title: 'REVIEW: ROCKY MOUNTAIN ALTITUDE 770 MSL', shortText: '‘Trail’ or ‘all-mountain’ is a matter of perspective',
+            size: 4, image: "BIKP-160200-TRAIL-051-640x360.jpg", date: 'Jan 01 2016'}
+    ];
+}
+
+function getCatalogData() {
+    return [
+        {title: 'Commencal', shortText: 'Commencal Bikes', size: 2, image: "52A77841-2-640x360.jpg"},
+        {title: 'Cube', shortText: 'Cube Bikes', size: 2, image: "52A77841-2-640x360.jpg"},
+        {title: 'Ghost', shortText: 'Ghost Bikes', size: 2, image: "52A77841-2-640x360.jpg"},
+        {title: 'Kona', shortText: 'Kona Bikes', size: 2, image: "52A77841-2-640x360.jpg"},
+        {title: 'NS Bikes', shortText: 'NS Bikes', size: 2, image: "52A77841-2-640x360.jpg"},
+        {title: 'Nukeproof', shortText: 'Nukeproof Bikes', size: 2, image: "52A77841-2-640x360.jpg"},
+        {title: 'Turner', shortText: 'Turner Bikes', size: 2, image: "52A77841-2-640x360.jpg"},
+        {title: 'Vitus Bikes', shortText: 'Vitus Bikes', size: 2, image: "52A77841-2-640x360.jpg"},
+        {title: 'Diamondback', shortText: 'Diamondback bikes', size: 2, image: "52A77841-2-640x360.jpg"},
+        {title: 'Giant', shortText: 'Giant Bikes', size: 2, image: "52A77841-2-640x360.jpg"},
+        {title: 'Ragley', shortText: 'Ragley Bikes', size: 2, image: "52A77841-2-640x360.jpg"}
+    ];
+}
+
 function init() {
     iniHelpers();
     initMenu();
@@ -11,6 +76,7 @@ function init() {
     initTraining();
     initBeginnersBlock();
     initAnimation();
+    initLogin();
 }
 
 function initMenu() {
@@ -59,11 +125,14 @@ function initMenuEvents(menu) {
 }
 
 function initAnimation() {
-    $('.filled-page').on("webkitTransitionEnd", function() {
-        var parentNode = this.parentNode;
-        parentNode.removeChild(this);
-        parentNode.appendChild(this);
-        $(this).removeClass('hidden');
+    $('.filled-page').on("webkitTransitionEnd", function(event) {
+        var parentNode;
+        if ($(event.target).hasClass('filled-page')) {
+            parentNode = this.parentNode;
+            parentNode.removeChild(this);
+            parentNode.appendChild(this);
+            $(this).removeClass('hidden');
+        }
     });
 }
 
@@ -72,119 +141,56 @@ function initLoginPopup() {
 }
 
 function initNewsGallery() {
-    var news = [
-        {title: 'REVIEW: ROCKY MOUNTAIN ALTITUDE 770 MSL', shortText: '‘Trail’ or ‘all-mountain’ is a matter of perspective',
-            size: 4, image: "BIKP-160200-TRAIL-051-640x360.jpg", date: 'Jan 01 2016'},
-        {title: 'DEITY: NEKO MULALLY', shortText: 'Mulally snags some liquid courage',
-            size: 2, image: 'Screen-Shot-2016-01-13-at-7.17.36-AM-320x180.png', date: 'Jan 01 2016'},
-        {title: 'COLD CASE', shortText: 'The country\'s first urban bike park seeks a second coming',
-            size: 2, image: "BIKP-150600-PARK-06-300x169.jpg", date: 'Jan 01 2016'},
-        {title: 'Trail', shortText: 'trail all mountain cross country womens all gear',
-            size: 2, image: "sidebar-bible-img.jpg", date: 'Jan 01 2016'},
-        {title: 'PURE METAL', shortText: 'Durango Bike Company\'s newest model isn\'t carbon',
-            size: 2, image: "ticker-15-300x169.jpg", date: 'Jan 01 2016'},
-        {title: 'Where’s the Love?', shortText: 'Why there are no hardtails in the Bible of Bike Tests',
-            size: 4, image: "52A77841-2-640x360.jpg", date: 'Jan 01 2016'},
-        {title: 'REVIEW: ROCKY MOUNTAIN ALTITUDE 770 MSL', shortText: '‘Trail’ or ‘all-mountain’ is a matter of perspective',
-            size: 4, image: "BIKP-160200-TRAIL-051-640x360.jpg", date: 'Jan 01 2016'},
-        {title: 'Trail', shortText: 'trail all mountain cross country womens all gear',
-            size: 2, image: "sidebar-bible-img.jpg", date: 'Jan 01 2016'},
-        {title: 'PURE METAL', shortText: 'Durango Bike Company\'s newest model isn\'t carbon',
-            size: 2, image: "ticker-15-300x169.jpg", date: 'Jan 01 2016'},
-        {title: 'Where’s the Love?', shortText: 'Why there are no hardtails in the Bible of Bike Tests',
-            size: 4, image: "52A77841-2-640x360.jpg", date: 'Jan 01 2016'},
-        {title: 'REVIEW: ROCKY MOUNTAIN ALTITUDE 770 MSL', shortText: '‘Trail’ or ‘all-mountain’ is a matter of perspective',
-            size: 4, image: "BIKP-160200-TRAIL-051-640x360.jpg", date: 'Jan 01 2016'}
-    ];
     var newsGallHandler = Handlebars.compile(document.getElementById('htNewsGallery').innerHTML);
-    document.getElementById('news').innerHTML += newsGallHandler({news: news, containerName: 'News'});
+    var news = getNewsData();
+    document.getElementById('news').innerHTML += newsGallHandler({relIndex: 1, news: news, subMenu: {
+            name: 'News',
+            menu: news
+        }
+    });
 }
 
 function initCatalog() {
-    var news = [
-        {title: 'REVIEW: ROCKY MOUNTAIN ALTITUDE 770 MSL', shortText: '‘Trail’ or ‘all-mountain’ is a matter of perspective',
-            size: 4, image: "BIKP-160200-TRAIL-051-640x360.jpg", date: 'Jan 01 2016'},
-        {title: 'DEITY: NEKO MULALLY', shortText: 'Mulally snags some liquid courage',
-            size: 2, image: 'Screen-Shot-2016-01-13-at-7.17.36-AM-320x180.png', date: 'Jan 01 2016'},
-        {title: 'COLD CASE', shortText: 'The country\'s first urban bike park seeks a second coming',
-            size: 2, image: "BIKP-150600-PARK-06-300x169.jpg", date: 'Jan 01 2016'},
-        {title: 'Trail', shortText: 'trail all mountain cross country womens all gear',
-            size: 2, image: "sidebar-bible-img.jpg", date: 'Jan 01 2016'},
-        {title: 'PURE METAL', shortText: 'Durango Bike Company\'s newest model isn\'t carbon',
-            size: 2, image: "ticker-15-300x169.jpg", date: 'Jan 01 2016'},
-        {title: 'Where’s the Love?', shortText: 'Why there are no hardtails in the Bible of Bike Tests',
-            size: 4, image: "52A77841-2-640x360.jpg", date: 'Jan 01 2016'},
-        {title: 'REVIEW: ROCKY MOUNTAIN ALTITUDE 770 MSL', shortText: '‘Trail’ or ‘all-mountain’ is a matter of perspective',
-            size: 4, image: "BIKP-160200-TRAIL-051-640x360.jpg", date: 'Jan 01 2016'},
-        {title: 'Trail', shortText: 'trail all mountain cross country womens all gear',
-            size: 2, image: "sidebar-bible-img.jpg", date: 'Jan 01 2016'},
-        {title: 'PURE METAL', shortText: 'Durango Bike Company\'s newest model isn\'t carbon',
-            size: 2, image: "ticker-15-300x169.jpg", date: 'Jan 01 2016'},
-        {title: 'Where’s the Love?', shortText: 'Why there are no hardtails in the Bible of Bike Tests',
-            size: 4, image: "52A77841-2-640x360.jpg", date: 'Jan 01 2016'},
-        {title: 'REVIEW: ROCKY MOUNTAIN ALTITUDE 770 MSL', shortText: '‘Trail’ or ‘all-mountain’ is a matter of perspective',
-            size: 4, image: "BIKP-160200-TRAIL-051-640x360.jpg", date: 'Jan 01 2016'}
-    ];
     var newsGallHandler = Handlebars.compile(document.getElementById('htNewsGallery').innerHTML);
-    document.getElementById('catalog').innerHTML += newsGallHandler({news: news, containerName: 'Catalog'});
+    var catalog = getCatalogData();
+    document.getElementById('catalog').innerHTML += newsGallHandler({relIndex: 2, news: catalog, subMenu: {
+        name: 'Catalog',
+        menu: catalog
+    }});
 }
 
 function initTraining() {
-    var news = [
-        {title: 'REVIEW: ROCKY MOUNTAIN ALTITUDE 770 MSL', shortText: '‘Trail’ or ‘all-mountain’ is a matter of perspective',
-            size: 4, image: "BIKP-160200-TRAIL-051-640x360.jpg", date: 'Jan 01 2016'},
-        {title: 'DEITY: NEKO MULALLY', shortText: 'Mulally snags some liquid courage',
-            size: 2, image: 'Screen-Shot-2016-01-13-at-7.17.36-AM-320x180.png', date: 'Jan 01 2016'},
-        {title: 'COLD CASE', shortText: 'The country\'s first urban bike park seeks a second coming',
-            size: 2, image: "BIKP-150600-PARK-06-300x169.jpg", date: 'Jan 01 2016'},
-        {title: 'Trail', shortText: 'trail all mountain cross country womens all gear',
-            size: 2, image: "sidebar-bible-img.jpg", date: 'Jan 01 2016'},
-        {title: 'PURE METAL', shortText: 'Durango Bike Company\'s newest model isn\'t carbon',
-            size: 2, image: "ticker-15-300x169.jpg", date: 'Jan 01 2016'},
-        {title: 'Where’s the Love?', shortText: 'Why there are no hardtails in the Bible of Bike Tests',
-            size: 4, image: "52A77841-2-640x360.jpg", date: 'Jan 01 2016'},
-        {title: 'REVIEW: ROCKY MOUNTAIN ALTITUDE 770 MSL', shortText: '‘Trail’ or ‘all-mountain’ is a matter of perspective',
-            size: 4, image: "BIKP-160200-TRAIL-051-640x360.jpg", date: 'Jan 01 2016'},
-        {title: 'Trail', shortText: 'trail all mountain cross country womens all gear',
-            size: 2, image: "sidebar-bible-img.jpg", date: 'Jan 01 2016'},
-        {title: 'PURE METAL', shortText: 'Durango Bike Company\'s newest model isn\'t carbon',
-            size: 2, image: "ticker-15-300x169.jpg", date: 'Jan 01 2016'},
-        {title: 'Where’s the Love?', shortText: 'Why there are no hardtails in the Bible of Bike Tests',
-            size: 4, image: "52A77841-2-640x360.jpg", date: 'Jan 01 2016'},
-        {title: 'REVIEW: ROCKY MOUNTAIN ALTITUDE 770 MSL', shortText: '‘Trail’ or ‘all-mountain’ is a matter of perspective',
-            size: 4, image: "BIKP-160200-TRAIL-051-640x360.jpg", date: 'Jan 01 2016'}
-    ];
     var newsGallHandler = Handlebars.compile(document.getElementById('htNewsGallery').innerHTML);
-    document.getElementById('training').innerHTML += newsGallHandler({news: news, containerName: 'Training'});
+    var news = getNewsData();
+    document.getElementById('training').innerHTML += newsGallHandler({relIndex: 3, news: news, subMenu: {
+        name: 'Training',
+        menu: news
+    }});
 }
 
 function initBeginnersBlock() {
-    var news = [
-        {title: 'REVIEW: ROCKY MOUNTAIN ALTITUDE 770 MSL', shortText: '‘Trail’ or ‘all-mountain’ is a matter of perspective',
-            size: 4, image: "BIKP-160200-TRAIL-051-640x360.jpg", date: 'Jan 01 2016'},
-        {title: 'DEITY: NEKO MULALLY', shortText: 'Mulally snags some liquid courage',
-            size: 2, image: 'Screen-Shot-2016-01-13-at-7.17.36-AM-320x180.png', date: 'Jan 01 2016'},
-        {title: 'COLD CASE', shortText: 'The country\'s first urban bike park seeks a second coming',
-            size: 2, image: "BIKP-150600-PARK-06-300x169.jpg", date: 'Jan 01 2016'},
-        {title: 'Trail', shortText: 'trail all mountain cross country womens all gear',
-            size: 2, image: "sidebar-bible-img.jpg", date: 'Jan 01 2016'},
-        {title: 'PURE METAL', shortText: 'Durango Bike Company\'s newest model isn\'t carbon',
-            size: 2, image: "ticker-15-300x169.jpg", date: 'Jan 01 2016'},
-        {title: 'Where’s the Love?', shortText: 'Why there are no hardtails in the Bible of Bike Tests',
-            size: 4, image: "52A77841-2-640x360.jpg", date: 'Jan 01 2016'},
-        {title: 'REVIEW: ROCKY MOUNTAIN ALTITUDE 770 MSL', shortText: '‘Trail’ or ‘all-mountain’ is a matter of perspective',
-            size: 4, image: "BIKP-160200-TRAIL-051-640x360.jpg", date: 'Jan 01 2016'},
-        {title: 'Trail', shortText: 'trail all mountain cross country womens all gear',
-            size: 2, image: "sidebar-bible-img.jpg", date: 'Jan 01 2016'},
-        {title: 'PURE METAL', shortText: 'Durango Bike Company\'s newest model isn\'t carbon',
-            size: 2, image: "ticker-15-300x169.jpg", date: 'Jan 01 2016'},
-        {title: 'Where’s the Love?', shortText: 'Why there are no hardtails in the Bible of Bike Tests',
-            size: 4, image: "52A77841-2-640x360.jpg", date: 'Jan 01 2016'},
-        {title: 'REVIEW: ROCKY MOUNTAIN ALTITUDE 770 MSL', shortText: '‘Trail’ or ‘all-mountain’ is a matter of perspective',
-            size: 4, image: "BIKP-160200-TRAIL-051-640x360.jpg", date: 'Jan 01 2016'}
-    ];
     var newsGallHandler = Handlebars.compile(document.getElementById('htNewsGallery').innerHTML);
-    document.getElementById('beginners').innerHTML += newsGallHandler({news: news, containerName: 'Beginners'});
+    var news = getNewsData();
+    document.getElementById('beginners').innerHTML += newsGallHandler({relIndex: 4, news: news, subMenu: {
+        name: 'Beginners',
+        menu: news
+    }});
+}
+
+function initLogin() {
+    var $tabs = $('.signin-header, .signup-header');
+    $tabs.on('click', function(event) {
+        var $target = $(this);
+        var $prevActive = $("#login .login-headers>.active");
+        if (!$target.hasClass('active')) {
+            $target.addClass('active')
+            $prevActive.removeClass('active');
+        }
+    });
+}
+
+function getRandomString() {
+    return Math.random().toString(36).slice(2);
 }
 
 function iniHelpers() {
@@ -200,4 +206,38 @@ function iniHelpers() {
             "%": lvalue % rvalue
         }[operator];
     });
+
+    Handlebars.registerHelper("columnLoop", function(context, options) {
+        var result = '';
+        var mInd;
+        var menuItem;
+        var stepItem;
+        var colNum = { 1: 8, 2: 4, 3: 2, 4: 2};
+        var curSize;
+        var stepInd;
+        var compile = Handlebars.compile(document.getElementById('htNewsGalleryBigItemColumn').innerHTML);
+        for (mInd = 0; mInd < context.length; mInd++) {
+            menuItem = context[mInd];
+            stepInd = mInd;
+            if (curSize !== menuItem.size) {
+                curSize = menuItem.size;
+                for(;mInd < stepInd + colNum[curSize] && mInd < context.length;mInd++) {
+                    stepItem = context[mInd];
+                    if (stepItem.size !== curSize) {
+                        break;
+                    }
+                }
+                mInd--;
+                curSize = null;
+            }
+            if (stepInd <= mInd) {
+                result += compile({items: context.slice(stepInd, mInd + 1)});
+            }
+        }
+        return result;
+    });
+
+    Handlebars.registerHelper("itemsColumn", function(context, options) {
+
+    })
 }
